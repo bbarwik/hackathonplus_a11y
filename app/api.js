8b4827c5412e0bbc5f17e6909c5cb6520aed5c70
bbarwik <bbarwik@gmail.com>
2019-11-24 11:40:08 +0100

    Retry request

--- app/api.js
+++ app/api.js
@@ -4,16 +4,21 @@ const REPORT_URL = "http://m.bbarwik.com/a11y/api.php";
 // TODO: Use Advanced API instead os simple API, also error handling
 
 class API {
-    send = (data) => {
+    send = async (data) => {
         data.session = Constants.sessionId;
-        fetch(REPORT_URL, {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-          });
+        while(true) {
+            let request = await fetch(REPORT_URL, {
+                method: 'POST',
+                headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            if(request.status == 200) {
+                break;
+            }
+        }
     }
 }
 
